9c2749a3db7d39573bf1682c1c4124d44e3083ff
KuwabataK <monorod98@gmail.com>
2021-01-27 01:04:22 +0900

    文言を変更

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="about">
     <h1>About</h1>
-    このサイトは非公式サイトであり、再生できる動画 /
-    音声は公式サイトとはなんの関係もありません。
+    このサイトは非公式サイトです。再生できる動画 / 音声の公開元企業/団体/個人との関係はありません
     <p />
     要望 / 不具合等ありましたらこちらまで →
     <a href="https://twitter.com/SuperSugerCocoa">Twitter</a>
