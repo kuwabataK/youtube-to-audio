f301f10b8788efc9d8a1c294cc83a26a0a478846
KuwabataK <monorod98@gmail.com>
2021-01-29 21:58:06 +0900

    Twitterを更新

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,7 +4,7 @@
     このサイトは非公式サイトです。再生できる動画 / 音声の公開元企業/団体/個人とは関係はありません
     <p />
     要望 / 不具合等ありましたらこちらまで →
-    <a href="https://twitter.com/SuperSugerCocoa">Twitter</a>
+    <a href="https://twitter.com/CocoaCurumi">Twitter</a>
     <p />
   </div>
 </template>
