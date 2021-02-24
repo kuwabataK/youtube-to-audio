97f0d3af155207d3afec57664efe2c2939b4996c
KuwabataK <monorod98@gmail.com>
2021-02-25 02:40:55 +0900

    Lintを適用

--- src/Hooks/useMouseOver.ts
+++ src/Hooks/useMouseOver.ts
@@ -3,7 +3,7 @@ import { stat } from "fs";
 
 export default function useMouseOver() {
   const state = reactive({
-    isMouseOver: false,
+    isMouseOver: false
   });
   const onMouseOver = () => {
     console.log("マウスが上に乗ったよ");
@@ -17,6 +17,6 @@ export default function useMouseOver() {
       return state.isMouseOver;
     }),
     onMouseOver,
-    onMouseLeave,
+    onMouseLeave
   };
 }
